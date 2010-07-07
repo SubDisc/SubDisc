41a6fcaabb354baefa5d3d497eec74d6f26ee08b
g_meeng <g_meeng>
2010-07-07 23:13:03 +0000

    ?
    git-svn-id: https://svn.science.uu.nl/repos/sci.knobb101.SubgroupDiscovery@311 3a85a776-1598-11dc-ad04-adf9e166afdd

--- SubDisc/src/nl/liacs/subdisc/DataCube.java
+++
@@ -1,9 +0,0 @@
-package nl.liacs.subdisc;
-
-public class DataCube
-{
-	protected int itsSize;
-
-	public int getSize() { return itsSize; }
-	public void setSize(int itsSize) { this.itsSize = itsSize; }
-}
