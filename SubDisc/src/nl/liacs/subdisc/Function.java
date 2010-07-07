741ebda9c7b4d38e0f5e563109e2ca8ff7dd813f
g_meeng <g_meeng>
2010-07-07 23:12:01 +0000

    New class not commited?
    git-svn-id: https://svn.science.uu.nl/repos/sci.knobb101.SubgroupDiscovery@310 3a85a776-1598-11dc-ad04-adf9e166afdd

---
+++ SubDisc/src/nl/liacs/subdisc/Function.java
@@ -0,0 +1,22 @@
+package nl.liacs.subdisc;
+
+public class Function
+{
+	// for LogGamma
+	private static final double GAMMA_STP = 2.50662827465;
+	private static final double GAMMA_C1 = 76.18009173;
+	private static final double GAMMA_C2 = -86.50532033;
+	private static final double GAMMA_C3 = 24.01409822;
+	private static final double GAMMA_C4 = -1.231739516;
+	private static final double GAMMA_C5 = 1.20858003e-3;
+	private static final double GAMMA_C6 = -5.36382e-6;
+
+	// uninstantiable
+	public Function() {}
+	
+	public static double logGamma(double x)
+	{
+		double ser = 1.0 + GAMMA_C1 / x + GAMMA_C2 / (x + 1.0) + GAMMA_C3 / (x + 2.0) + GAMMA_C4 / (x + 3.0) + GAMMA_C5 / (x + 4.0) + GAMMA_C6 / (x + 5.0);
+		return (x - 0.5) * Math.log(x + 4.5) - x - 4.5 + Math.log(GAMMA_STP * ser);
+	}
+}
