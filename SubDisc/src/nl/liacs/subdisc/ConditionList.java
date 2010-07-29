b13074e0eb2f2dfb00c059b10a7bf89ec28b0f1c
Arno Knobbe <knobbeaj>
2010-07-29 09:14:13 +0000

    eerste implementatie van ConditionList.equals(). Nog niet getest en nog niet volledig.
    git-svn-id: https://svn.science.uu.nl/repos/sci.knobb101.SubgroupDiscovery@330 3a85a776-1598-11dc-ad04-adf9e166afdd

--- SubDisc/src/nl/liacs/subdisc/ConditionList.java
+++ SubDisc/src/nl/liacs/subdisc/ConditionList.java
@@ -33,6 +33,36 @@ public class ConditionList
 		return aResult;
 	}
 
+	//this method computes logical equivalence. This means that the actual number of conditions or the order may differ.
+	//Just as long as it effectively selects the same subgroup, no matter what the database is.
+	//This method currently doesn't consider equavalence of the type a<10&a<20 vs. a<10 etc.
+	public boolean equals(Object theObject)
+	{
+		if (theObject.getClass() != this.getClass())
+			return false;
+		ConditionList aCL = (ConditionList) theObject;
+
+		//check in one direction
+		for (Condition aCondition : aCL.itsConditions)
+			if (!findCondition(aCondition))
+				return false;
+
+		//check in the other direction
+		for (Condition aCondition : itsConditions)
+			if (!aCL.findCondition(aCondition))
+				return false;
+
+		return true;
+	}
+
+	public boolean findCondition(Condition theCondition)
+	{
+		for (Condition aCondition : itsConditions)
+			if (theCondition.equals(aCondition))
+				return true;
+		return false;
+	}
+
 	public boolean addCondition(Condition aCondition)
 	{
 		if (itsConditions.indexOf(aCondition) == -1) //already present?
