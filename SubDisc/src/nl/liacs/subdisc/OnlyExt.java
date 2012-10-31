ec9077bfc381fe0919ccc83d3597fd74330bef48
Rob Konijn <konijn>
2012-10-31 14:20:12 +0000

    nog 1 class file vergeten
    git-svn-id: https://svn.science.uu.nl/repos/sci.knobb101.SubgroupDiscovery@1443 3a85a776-1598-11dc-ad04-adf9e166afdd

---
+++ SubDisc/src/nl/liacs/subdisc/OnlyExt.java
@@ -0,0 +1,13 @@
+package nl.liacs.subdisc;
+import java.io.*;
+public class OnlyExt implements FilenameFilter {
+ 
+	String ext;
+	public OnlyExt(String ext) {
+	this.ext = "." + ext;
+	}
+	public boolean accept(File dir, String name) {
+	return name.endsWith(ext);
+	}
+		
+}
