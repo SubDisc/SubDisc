5312920e9ca11edb3d599fadfceffd1b424b5f90
g_meeng <g_meeng>
2013-09-20 11:28:02 +0000

    Operator: boolean isValidFor(AttributeType), tests whether Operator is valid for supplied AttributeType. This unifies some Condition/Column/Operator checking. Implemented as abstract method, such that every (new) Operator is enforced to declare the valid types.
    git-svn-id: https://svn.science.uu.nl/repos/sci.knobb101.SubgroupDiscovery@2056 3a85a776-1598-11dc-ad04-adf9e166afdd

--- SubDisc/src/nl/liacs/subdisc/Operator.java
+++ SubDisc/src/nl/liacs/subdisc/Operator.java
@@ -4,61 +4,65 @@ import java.util.*;
 
 public enum Operator
 {
-//	public static final int ELEMENT_OF		= 1;
-//	public static final int EQUALS			= 2;
-//	public static final int LESS_THAN_OR_EQUAL	= 3;
-//	public static final int GREATER_THAN_OR_EQUAL	= 4;
-//	public static final int BETWEEN = 5;
-//	public static final int NOT_AN_OPERATOR		= 99;
-//
-//	// Binary Operator Constants
-//	public static final int FIRST_BINARY_OPERATOR	= EQUALS;
-//	public static final int LAST_BINARY_OPERATOR	= EQUALS;
-//
-//	// Nominal Operator  Constants
-//	public static final int FIRST_NOMINAL_OPERATOR	= ELEMENT_OF;
-//	public static final int LAST_NOMINAL_OPERATOR	= EQUALS;
-//
-//	// Numeric Operator  Constants
-//	//this allows =, <= and >=
-//	public static final int FIRST_NUMERIC_OPERATOR	= EQUALS;
-//	public static final int LAST_NUMERIC_OPERATOR	= BETWEEN;
-
-//	public boolean hasNextOperator()
-//	{
-//		if (itsOperator == LAST_BINARY_OPERATOR && itsColumn.isBinaryType())
-//			return false;
-//		if (itsOperator == LAST_NOMINAL_OPERATOR && itsColumn.isNominalType())
-//			return false;
-//		if (itsOperator == LAST_NUMERIC_OPERATOR && itsColumn.isNumericType())
-//			return false;
-//		return true;
-//	}
-//
-//	public int getNextOperator()
-//	{
-//		return hasNextOperator() ? itsOperator+1 : NOT_AN_OPERATOR;
-//	}
-
-//	private String getOperatorString()
-//	{
-//		switch(itsOperator)
-//		{
-//			case ELEMENT_OF		: return "in";
-//			case EQUALS			: return "=";
-//			case LESS_THAN_OR_EQUAL		: return "<=";
-//			case GREATER_THAN_OR_EQUAL	: return ">=";
-//			case BETWEEN	: return "in";
-//			default : return "";
-//		}
-//	}
-
-	ELEMENT_OF("in"),
-	EQUALS("="),
-	LESS_THAN_OR_EQUAL("<="),
-	GREATER_THAN_OR_EQUAL(">="),
-	BETWEEN("in"),
-	NOT_AN_OPERATOR("");	// may not be needed anymore
+	ELEMENT_OF("in")
+	{
+		@Override
+		public boolean isValidFor(AttributeType theType)
+		{
+			return theType == AttributeType.NOMINAL;
+		}
+	},
+	EQUALS("=")
+	{
+		@Override
+		public boolean isValidFor(AttributeType theType)
+		{
+			return theType == AttributeType.NOMINAL ||
+				theType == AttributeType.NUMERIC ||
+				// ORDINAL is not implemented -> do not use
+				// all code should be checked when it is
+				theType == AttributeType.BINARY;
+		}
+	},
+	LESS_THAN_OR_EQUAL("<=")
+	{
+		@Override
+		public boolean isValidFor(AttributeType theType)
+		{
+			// ORDINAL is not implemented -> do not use
+			// all code should be checked when it is
+			return theType == AttributeType.NUMERIC;
+		}
+	},
+	GREATER_THAN_OR_EQUAL(">=")
+	{
+		@Override
+		public boolean isValidFor(AttributeType theType)
+		{
+			// ORDINAL is not implemented -> do not use
+			// all code should be checked when it is
+			return theType == AttributeType.NUMERIC;
+		}
+	},
+	BETWEEN("in")
+	{
+		@Override
+		public boolean isValidFor(AttributeType theType)
+		{
+			// ORDINAL is not implemented -> do not use
+			// all code should be checked when it is
+			return theType == AttributeType.NUMERIC;
+		}
+	},
+	@Deprecated
+	NOT_AN_OPERATOR("") // legacy, will be removed in some future update
+	{
+		@Override
+		public boolean isValidFor(AttributeType theType)
+		{
+			return true;
+		}
+	};
 
 	public final String GUI_TEXT;
 
@@ -66,6 +70,16 @@ public enum Operator
 	{
 		GUI_TEXT = theGuiText;
 	}
+
+	// abstract to enforce implementation for each (new) Operator
+	/**
+	 * Indicates whether this Operator is valid for the supplied
+	 * {@link AttributeType}.
+	 * 
+	 * @return {@code true} if this {@code Operator} is valid for the
+	 * supplied {@link AttributeType}.
+	 */
+	abstract public boolean isValidFor(AttributeType theType);
 
 	@Override
 	public String toString()
